7ebd716e405487aefaa1994cfa2491eb372a0bff
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-12 10:34:59 -0400

    commit tree f51c748433169371bbc99136ef13bcb6bd278f36

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "N/A"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "N/A"},
+    //...
+  ]
+
+
+
+function superbowlWin(array){
+    let winningElement =  array.find(element => element.result === "W")
+    if(winningElement){
+       return winningElement.year
+    }
+}
+
+console.log(superbowlWin(record));
+
